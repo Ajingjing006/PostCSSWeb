b09e27a00d82d22968cfc5200ed1ba629d65b884
Robert <houtong_an@163.com>
2021-05-31 01:30:23 +0800

    study notes of ch14_cssnext

---
+++ ch14_cssnext/demo_4/gulpfile.js
@@ -0,0 +1,68 @@
+'use strict';
+var gulp     = require('gulp');
+var postcss = require('gulp-postcss');
+var rename = require('gulp-rename');
+var nano = require('gulp-cssnano');
+var stylelint = require('stylelint');
+var sourcemaps = require('gulp-sourcemaps');
+//var autoprefixer = require('autoprefixer');
+var reporter = require('postcss-reporter');
+var rebeccaPurple = require('postcss-color-rebeccapurple');
+
+var stylerules = {
+  "color-no-invalid-hex": 2,
+  "declaration-colon-space-before": [2, "never"],
+  "indentation": [2, 2],
+  "number-leading-zero": [2, "always"]
+};
+
+var renameFunction = function (path) {
+  path.extname = ".min.css";
+  return path;
+};
+
+var sourceMapLocation = ['dest/*.css', '!dest/*.min.css'];
+
+gulp.task('styles', function () {
+  return gulp.src('src/*.css')
+    .pipe(postcss([ rebeccaPurple() ]))
+    .pipe(gulp.dest('dest/'));
+});
+
+gulp.task('lint-styles', function() {
+  return gulp.src("dest/*.css")
+    .pipe(postcss([ stylelint({ "rules": stylerules }), 
+    reporter({ clearMessages: true })
+  ]))
+});
+
+gulp.task('rename', function () {
+  return gulp.src('dest/*.css')
+    .pipe(rename(renameFunction))
+    .pipe(gulp.dest("dest/"));
+});
+
+
+gulp.task('minifyCSS', function () {
+  return gulp.src('dest/*.min.css')
+    .pipe(nano({ autoprefixer: false }))
+    .pipe(gulp.dest("dest/"));
+});
+
+gulp.task('sourcemap', function () {
+  return gulp.src(sourceMapLocation)
+    .pipe(sourcemaps.init())
+    .pipe(sourcemaps.write('maps/', {
+      sourceMappingURLPrefix: 'https://www.mydomain.com/'
+    }))
+    .pipe(gulp.dest("dest/"));
+});
+
+gulp.task('watch', function(){
+  gulp.watch('src/*.css', gulp.series('default'))
+      .on('change', function(event){
+          console.log('File '+event+' was changed, running tasks...');
+  })
+});
+
+gulp.task('default', gulp.series('styles', 'lint-styles', 'rename', 'minifyCSS', 'sourcemap', 'watch'));
---
+++ ch14_cssnext/demo_7/postcss-backgroundblend/index.js
@@ -0,0 +1,19 @@
+var postcss = require('postcss');
+ 
+module.exports = postcss.plugin('backgroundblend', function backgroundblend(options) {
+ 
+    return function (css) {
+ 
+        options = options || {};
+         
+        // Processing code will be added here
+        //css.eachDecl('background-blend', function (decl) {
+        css.walkRules(function (rule) {
+          rule.walkDecls(function(decl) {
+            if (decl.prop.indexOf('background-blend') !== -1) {
+              decl.prop  = 'background-blend-mode';
+            }
+          })
+    });
+  };
+});
